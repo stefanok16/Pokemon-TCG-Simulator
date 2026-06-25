53386ce83a12f474e840a8ab34cc6f995bdf3ed7
unknown <stefanokatsavounidis@gmail.com>
2026-06-25 13:55:32 -0400

    load decks

--- deckmanagement.py
+++ deckmanagement.py
@@ -1,9 +1,18 @@
 #The deck management (create, delete, edit). Pandas for csv handling
 
 import pandas as pd 
+import os
+import glob
+
+#load in decks folder 
+path = os.getcwd()
+decks = glob.glob(os.path.join(path, "*.csv"))
+
+#loop through continues to load csv into memory
 
 try:
-    pd.read_csv("./decks")
-    
+    for deck in decks:
+        deck = pd.read_csv(deck)
+
 except:
-    AttributeError(" 'decks' folder may have been deleted.")
+    AttributeError(" 'decks' folder may have been deleted")
